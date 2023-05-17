4a48a8a93b2f6694fc3473fcbb41e4db887e9fc8
harel <harel_shalom@yahoo.com>
2023-05-18 01:26:55 +0300

    finish navbar design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,16 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
 function App() {
   return (
     <>
-      <h1>Wellcom to my Store</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Wellcom to my Store</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 }
